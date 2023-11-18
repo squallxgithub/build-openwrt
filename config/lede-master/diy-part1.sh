691836a4b525056ea8d6ac648db361668af322c2
squallxgithub <18038165@qq.com>
2023-11-18 19:52:12 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -11,6 +11,9 @@
 #sed -i '$a src-git chinadns_ng https://github.com/immortalwrt/package.git' feeds.conf.default
 #sed -i '$a src-git luci_passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git repwrtbwmon https://github.com/brvphoenix/wrtbwmon' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
